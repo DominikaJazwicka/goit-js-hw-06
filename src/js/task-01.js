7c55fb8f81f22943ef68103a0355bbd108d10897
DominikaJazwicka <109682344+DominikaJazwicka@users.noreply.github.com>
2023-02-21 00:06:57 +0100

    main

---
+++ src/js/task-01.js
@@ -0,0 +1,13 @@
+const listElement = document.querySelectorAll("h2");
+
+for(let key of listElement) {
+    console.log(key)
+}
+
+const quantity = listElement.length;
+
+console.log(quantity);
+console.log(`Numbers of categories ${quantity}`)
+
+
+
